7d049444c506ade521174e6b634f070ca9190dfd
edwardymr <ymartinez2109@gmail.com>
2025-09-16 05:29:31 -0500

    arregalndo el supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+// src/lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL y/o Anon Key no están configurados");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
